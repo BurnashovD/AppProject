2b9e3fa1701b95877cb23db512d7a4fe3bb61766
Daniil Burnashov <danil-burnashov@mail.ru>
2022-10-08 16:46:30 +0300

    Исправленное дз

---
+++ AppProject/AppProject/TabBar/BuyViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BuyViewController.swift
+//  AppProject
+//
+//  Created by Daniil on 08.10.2022.
+//
+
+import UIKit
+
+// Класс покупки ??
+final class BuyViewController: UIViewController {
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       configUI()
+    }
+    
+    // MARK: - Private methods
+    private func configUI() {
+        view.backgroundColor = .black
+    }
+}
---
+++ AppProject/AppProject/TabBar/MainTabBarViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabBarViewController.swift
+//  AppProject
+//
+//  Created by Daniil on 08.10.2022.
+//
+
+import UIKit
+
+// Класс конфигуратор для TabBar
+final class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTabBarController()
+    }
+    
+    private func configTabBarController() {
+        let buyVC = BuyViewController()
+        let searchVC = SearchViewController()
+        let forMeVC = ForMeViewController()
+        let cartVC = CartViewController()
+        
+        buyVC.tabBarController?.tabBar.backgroundColor = UIColor(red: 0.12, green: 0.12, blue: 0.12, alpha: 1.00)
+        buyVC.tabBarItem = UITabBarItem(title: "Купить", image: UIImage(systemName: "laptopcomputer.and.iphone"), tag: 2)
+        
+        searchVC.tabBarItem = UITabBarItem(title: "Поиск", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        searchVC.tabBarController?.tabBar.backgroundColor = UIColor(red: 0.07, green: 0.07, blue: 0.07, alpha: 1.00)
+        
+        forMeVC.tabBarItem = UITabBarItem(title: "Для Вас", image: UIImage(systemName: "person.circle"), tag: 1)
+        forMeVC.tabBarController?.tabBar.backgroundColor = UIColor(red: 0.12, green: 0.12, blue: 0.12, alpha: 1.00)
+        
+        cartVC.title = "Коризна"
+        cartVC.tabBarController?.tabBar.backgroundColor = UIColor(red: 0.12, green: 0.12, blue: 0.12, alpha: 1.00)
+        cartVC.tabBarItem = UITabBarItem(title: "Для Вас", image: UIImage(systemName: "bag"), tag: 1)
+        
+        let navigationController = UINavigationController(rootViewController: searchVC)
+        
+        viewControllers = [buyVC, forMeVC, navigationController, cartVC]
+    }
+    
+}
