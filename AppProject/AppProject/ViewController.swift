32f1f09cd84f35655773411f0c2ed62be28047dc
Daniil Burnashov <danil-burnashov@mail.ru>
2022-10-08 16:33:05 +0300

    init

---
+++ AppProject/AppProject/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AppProject
+//
+//  Created by Daniil on 08.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
